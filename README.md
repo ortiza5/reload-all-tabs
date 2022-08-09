6866c87196ed01738899c30c8428ad6ba660d3fc
ortiza5 <43008712+ortiza5@users.noreply.github.com>
2022-08-09 17:12:43 -0600

    Remove all traces of select all tabs function

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
-# Reload and Select All Tabs Browser Extension:
+# Reload All Tabs Browser Extension:
 ![icon](/icons/48.png)
 
 A chromium based browser extension created for [@Vivipop](https://forum.vivaldi.net/user/vivipop) on the Vivaldi Browser Forums.
 
 ## Features
-* Click the extension icon to either select all tabs or reload all tabs (configurable in options with both actions available in the extension button's context menu).
-* Keyboard shortcut option to perform the same actions.
+* Click the extension icon to reload all tabs in the current window
+* Keyboard shortcut option to perform the same action
   * Make sure to set the keyboard shortcuts to `Global` instead of `in Vivaldi` to get them to work
 
 ## Installation
--- background.js
+++ background.js
@@ -1,34 +1,10 @@
 // Set an initial value for settings on install
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.local.set({ clickAction: "refresh-tabs" }, function () {
-    console.log("Initial setting set...");
-  });
-});
-
 function refreshAllTabs() {
   // Get all tabs
   chrome.tabs.query({ currentWindow: true }, (tabs) => {
     tabs.forEach((tab) => {
       chrome.tabs.reload(tab.id);
     });
-
-    // notify completion
-    flashBadge("✓", 3000);
-  });
-}
-
-function selectAllTabs() {
-  // Get all tabs
-  chrome.tabs.query({ currentWindow: true }, (tabs) => {
-    chrome.tabs.highlight({ tabs: Array.from(Array(tabs.length).keys()) });
-
-    // tabs.forEach((tab) => {
-    //   chrome.tabs.update(tab.id, { selected: true });
-    // });
-
-    // tabs.forEach((tab) => {
-    //   chrome.tabs.update(tab.id, { highlighted: true });
-    // });
 
     // notify completion
     flashBadge("✓", 3000);
@@ -47,29 +23,10 @@ function flashBadge(symbol, duration) {
 chrome.commands.onCommand.addListener((command) => {
   if (command === "refresh-tabs") {
     refreshAllTabs();
-  } else if (command === "select-tabs") {
-    // BUG: selecting doesn't work, refresh only for now
-    refreshAllTabs();
-    // selectAllTabs();
   }
 });
 
 // Extension icon click listener
 chrome.action.onClicked.addListener(() => {
-  chrome.storage.local.get({ clickAction: "refresh-tabs" }, (items) => {
-    const action = items.clickAction;
-
-    if (action === "refresh-tabs") {
-      refreshAllTabs();
-    } else if (action === "select-tabs") {
-      // BUG: selecting doesn't work, refresh only for now
-      refreshAllTabs();
-      // selectAllTabs();
-    }
-  });
+  refreshAllTabs();
 });
-
-// Open the options on install
-// chrome.runtime.onInstalled.addListener(() => {
-//   chrome.runtime.openOptionsPage();
-// });
